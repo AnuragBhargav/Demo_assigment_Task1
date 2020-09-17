82558fa7adf3de3b31e9f4c6942239bbc7a16ea9
AnuragBhargav <40573077+AnuragBhargav@users.noreply.github.com>
2020-09-17 13:08:57 +0530

    Update demo.py

--- code/demo.py
+++ code/demo.py
@@ -1,1 +1,13 @@
-print("Hello")
+from flask import Flask, render_template, request, jsonify
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+   return "Hello Master Branch"
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9092)
